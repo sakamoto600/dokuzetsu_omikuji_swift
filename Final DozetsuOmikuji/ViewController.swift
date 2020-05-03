ab28c5d5dcf659d51ad5002adf38101fd62807ce
Ryota Sakamoto <sakamoto@apple.com>
2020-05-04 02:08:21 +0900

    Initial Commit

---
+++ Final DozetsuOmikuji/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Final DozetsuOmikuji
+//
+//  Created by Ryota Sakamoto on 2020/05/04.
+//  Copyright © 2020 Ryota Sakamoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
